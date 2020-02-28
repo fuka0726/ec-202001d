3f7099e0def52b074cf4fc281917f5fb456ac9f4
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-02-28 15:54:40 +0900

    first commit

---
+++ src/main/java/com/example/Ec202001dApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202001dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202001dApplication.class, args);
+	}
+
+}
