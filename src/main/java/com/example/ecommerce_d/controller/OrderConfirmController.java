06bd21bd18b41479816eba65e36af3728aca9e51
shumpeiyuguchi <shumpei.yuguchi@rakus-partners.co.jp>
2020-03-07 14:38:09 +0900

    注文確認コントローラでフォームをセットアップしました

--- src/main/java/com/example/ecommerce_d/controller/OrderConfirmController.java
+++ src/main/java/com/example/ecommerce_d/controller/OrderConfirmController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.ecommerce_d.domain.LoginUser;
@@ -29,14 +30,18 @@ import com.example.ecommerce_d.service.OrderConfirmService;
  *
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class OrderConfirmController {
 
 	@Autowired
 	private OrderConfirmService orderConfirmService;
 
+	@ModelAttribute
+	public OrderForm setUpOrderForm() {
+		return new OrderForm();
+	}
+
 	/**
-	 * 
 	 * 注文前の商品を取得し、注文確認ページに画面遷移
 	 * 
 	 * @param userId ユーザーid
@@ -58,35 +63,36 @@ public class OrderConfirmController {
 	 * @param form      お届け先情報
 	 * @param resultset エラーメッセージを格納するオブジェクト
 	 * @param userId    ユーザーid
-	 * @param model		リクエストスコープ
+	 * @param model     リクエストスコープ
 	 * @param loginUser 利用者ユーザー
 	 * @return 注文完了画面（リダイレクト）
 	 */
+	//TODO 注文がアップデートされない
 	@RequestMapping("/completeOrder")
 	public String completeOrder(@Validated OrderForm form, BindingResult resultset, Integer userId, Model model,
 			@AuthenticationPrincipal LoginUser loginUser) {
-		
+
 		if ("".equals(form.getDeliveryDate())) {
 			FieldError deliveryDateError = new FieldError(resultset.getObjectName(), "deliveryDate", "配達日を入力してください");
 			resultset.addError(deliveryDateError);
 		}
-		//エラーがある場合は注文確認画面に遷移
+		// エラーがある場合は注文確認画面に遷移
 		if (resultset.hasErrors()) {
 			System.out.println(resultset);
 			return orderConfirm(userId, model, loginUser);
 		}
-		//配達日をSQL用のTimestamp型に変更
+		// 配達日をSQL用のTimestamp型に変更
 		Date date = Date.valueOf(form.getDeliveryDate());
 		LocalDate localdate = date.toLocalDate();
 		LocalTime localTime = LocalTime.of(Integer.parseInt(form.getDeliveryTime()), 00);
 		LocalDateTime localDateTime = LocalDateTime.of(localdate, localTime);
 		System.out.println(localDateTime);
 		Timestamp timestamp = Timestamp.valueOf(localDateTime);
-		//Orderインスタンスを作成(テーブルからとってきた方が良いのかしら？）
+		// Orderインスタンスを作成(テーブルからとってきた方が良いのかしら？）
 		Order order = new Order();
 		BeanUtils.copyProperties(form, order);
 		order.setDeliveryTime(timestamp);
-		//支払方法によって入金情報を変更
+		// 支払方法によって入金情報を変更
 		if (order.getPaymentMethod() == 1) {
 			order.setStatus(1);
 		} else {
