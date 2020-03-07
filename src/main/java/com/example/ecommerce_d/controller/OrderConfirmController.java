c8feeb81342785abab7e8530da40fb6897df456c
tsubasa1111 <namiki.ba13@gmail.com>
2020-03-07 21:05:21 +0900

    メール送付機能追加

--- src/main/java/com/example/ecommerce_d/controller/OrderConfirmController.java
+++ src/main/java/com/example/ecommerce_d/controller/OrderConfirmController.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.ecommerce_d.domain.LoginUser;
 import com.example.ecommerce_d.domain.Order;
+import com.example.ecommerce_d.domain.OrderItem;
+import com.example.ecommerce_d.domain.OrderTopping;
 import com.example.ecommerce_d.form.OrderForm;
 import com.example.ecommerce_d.service.OrderConfirmService;
 
@@ -36,10 +40,14 @@ public class OrderConfirmController {
 	@Autowired
 	private OrderConfirmService orderConfirmService;
 
+	@Autowired
+    private MailSender sender;
+	
 	@ModelAttribute
 	public OrderForm setUpOrderForm() {
 		return new OrderForm();
 	}
+	
 
 	/**
 	 * 注文前の商品を取得し、注文確認ページに画面遷移
@@ -67,7 +75,7 @@ public class OrderConfirmController {
 	 * @param loginUser 利用者ユーザー
 	 * @return 注文完了画面（リダイレクト）
 	 */
-	//TODO 注文がアップデートされない
+	//TODO 注文がアップデートされない(改修済み)
 	@RequestMapping("/completeOrder")
 	public String completeOrder(@Validated OrderForm form, BindingResult resultset, Integer userId, Model model,
 			@AuthenticationPrincipal LoginUser loginUser) {
@@ -85,7 +93,6 @@ public class OrderConfirmController {
 		LocalDate localdate = date.toLocalDate();
 		LocalTime localTime = LocalTime.of(Integer.parseInt(form.getDeliveryTime()), 00);
 		LocalDateTime localDateTime = LocalDateTime.of(localdate, localTime);
-		System.out.println(localDateTime);
 		Timestamp timestamp = Timestamp.valueOf(localDateTime);
 		// Orderインスタンスを作成(テーブルからとってきた方が良いのかしら？）
 		Order order = new Order();
@@ -98,9 +105,14 @@ public class OrderConfirmController {
 		} else {
 			order.setStatus(2);
 		}
-		System.out.println(order.getPaymentMethod());
+		//注文確認メールに注文詳細を記載するためにsql発行
+//		List<Order> orderList = orderConfirmService.showOrderedList(loginUser.getUser().getId());
 		orderConfirmService.updateStatus(order); 
-//		userRepository.update(order);
+		
+		
+		//メール送信するメソッドを呼ぶ.
+//		sendEmail(order,orderList);
+		
 		return "redirect:/tocomplete";
 	}
 
@@ -108,5 +120,78 @@ public class OrderConfirmController {
 	public String tocomplete() {
 		return "order_finished";
 	}
+	
+	
+//以下メール送付のコード
+	
+//	/**
+//	 * 
+//	 * 注文内容をメールで送る
+//	 * 
+//	 * @param order　お届け情報
+//	 * @param orderList　注文リスト
+//	 */
+//	public void sendEmail(Order order,List<Order> orderList) {
+//		//改行のためのメソッド
+//		String br = System.getProperty("line.separator");
+//		String itemName=null;
+//		Character itemSize=null;
+//		Integer itemCount=null;
+//		
+//		//注文内容を変数に代入
+//		List<OrderTopping> toppingList=null;
+//		for(Order orderItem:orderList) {
+//			for(OrderItem item:orderItem.getOrderItemList()) {
+//		     itemName=item.getItem().getName();
+//		     itemSize=item.getSize();
+//		     itemCount=item.getQuantity();
+//		     toppingList=item.getOrderToppingList();
+//			}
+//		}
+//
+//		//トッピングを文字連結する
+//		String toppingName="";
+//	    for(OrderTopping topping:toppingList) {
+//		toppingName=toppingName+topping.getTopping().getName()+",";
+//	    }
+//	    
+//	    String paymentMethod=null;
+//	    if(order.getPaymentMethod()==1) {
+//	    	paymentMethod="代引決済";
+//	    }else {
+//	    	paymentMethod="クレジットカード";
+//	    }
+//	    
+//	    //メールの本文
+//		String message=order.getDestinationName()+"様"+br+br+"この度は発注いただきありがとうございました。"
+//				+ br+"ご注文内容を以下のとおりお知らせいたします。"+br+
+//				"内容にお間違いがないかご確認の程よろしくお願いいたします。"+ br+br+
+//				     "【注文日】："+order.getOrderDate()+br+
+//				     "【商品詳細】："+br+
+//				     "・商品名："+itemName+br+
+//				     "・サイズ："+itemSize+br+
+//				     "・個数："+itemCount+br+
+//				     "・トッピング："+toppingName+br+br+
+//
+//				     "【商品代金】："+order.getTotalPrice()+"円"+br+
+//				     "【お届け先】："+order.getDestinationAddress()+br+
+//					 "【配達日時】："+order.getDeliveryTime()+br+
+//					 "【決済方法】："+paymentMethod+br+br+
+//					 "株式会社ラクラクトイザラス";
+//		
+//		//メールを送る準備
+//		SimpleMailMessage msg = new SimpleMailMessage();
+//	
+//		//送り主
+//		msg.setFrom("namiki.ba13@gmail.com");
+//		//宛先
+//		msg.setTo("namiki.ba13@gmail.com");//order.getDestinationEmail() を本来入れる
+//		msg.setSubject("ご注文完了のお知らせ");
+//		//本文
+//		msg.setText(message);
+//		
+//		this.sender.send(msg);
+//		
+//	}
 
 }
